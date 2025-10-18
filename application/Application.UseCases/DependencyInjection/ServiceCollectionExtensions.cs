86ac36c71f6eaec7b034f9eb50bfaff602b2c326
Ig0rPaim <igorpaimdeoliveira@gmail.com>
2025-10-18 00:42:18 -0300

    init mediatR implementation

---
+++ application/Application.UseCases/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,6 @@
+namespace Application.UseCases.DependencyInjection;
+
+public class ServiceCollectionExtensions
+{
+    
+}
---
+++ infra/web/Controllers/UserController.cs
@@ -0,0 +1,6 @@
+namespace CleanArch.Controllers;
+
+public class UserController
+{
+    
+}
